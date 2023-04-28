e825a0fef7fa954ea88747b1286be449719529d5
Idelmario Macedo <idelmariocorreia@hotmail.com>
2023-04-28 12:29:19 -0300

    feat: Create RepoBox component  On branch main  Changes to be committed: 	new file:   src/components/RepoBox.tsx

---
+++ src/components/RepoBox.tsx
@@ -0,0 +1,14 @@
+
+export function RepoBox () {
+
+  return (
+    <div className="h-auto w-auto p-4 rounded-lg bg-gray-900">
+      <div> 
+        <strong className="text-gray-300" >react-unit-testing-jest</strong>
+        <p className="text-sm text-gray-500">Repositório destinado à pratica de testes em react</p>
+      </div>
+
+      <button>Like</button>
+    </div>
+  )
+}
