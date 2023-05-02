3d114b7fedf3b05a3064b2b1ffc2eb89ffefe282
Idelmario Macedo <idelmariocorreia@hotmail.com>
2023-05-02 08:17:02 -0300

    feat: Add styling to RepoBox  On branch main  Changes to be committed: 	modified:   src/components/RepoBox.tsx

--- src/components/RepoBox.tsx
+++ src/components/RepoBox.tsx
@@ -1,14 +1,19 @@
+interface Repo {
+  name: string;
+  description: string;
+}
 
-export function RepoBox () {
+export function RepoBox ({ name, description }:Repo ) {
 
   return (
-    <div className="h-auto w-auto p-4 rounded-lg bg-gray-900">
-      <div> 
-        <strong className="text-gray-300" >react-unit-testing-jest</strong>
-        <p className="text-sm text-gray-500">Repositório destinado à pratica de testes em react</p>
+    <div className="flex flex-col items-start justify-between h-36 w-52 p-4 rounded-lg bg-[#202024] hover:border-2 hover:border-[#9B4DEE]">
+      <div className="group w-44 "> 
+        <strong className="text-gray-200" >{name}</strong>
+        <p className="text-sm truncate text-gray-500 pt-2">{description}</p>
+        <span className="absolute hidden w-44 h-auto rounded p-2 font-normal text-gray-300 bg-[#202024] border-2 border-[#9b4dee] group-hover:block">{description}</span>
       </div>
 
-      <button>Like</button>
+      <button className="">Like</button>
     </div>
   )
 }
